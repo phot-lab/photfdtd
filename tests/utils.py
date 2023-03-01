b0fe2aa848a193763ddd9601c908181b9dc938bc
Chunyu Li <cyli0212@gmail.com>
2023-03-01 10:40:01 +0800

    Modified utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,6 @@ import sys
 from pathlib import Path
 
 path = Path()
-parent_directory = path.parent.absolute() # 获取父目录
+parent_directory = path.parent.absolute()  # 获取父目录
 
-sys.path.append("/Users/lichunyu/VSCode/PassiveOpticalDevices") # 将父目录加入 Python 路径
+sys.path.append(parent_directory)  # 将父目录加入 Python 路径
