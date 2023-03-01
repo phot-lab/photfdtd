9a212aaf9c1302a930b55db6ffd5a8474da9788d
Chunyu Li <cyli0212@gmail.com>
2023-03-01 10:58:33 +0800

    Modified utils.py again

--- tests/utils.py
+++ tests/utils.py
@@ -4,4 +4,4 @@ from pathlib import Path
 path = Path()
 parent_directory = path.parent.absolute()  # 获取父目录
 
-sys.path.append(parent_directory)  # 将父目录加入 Python 路径
+sys.path.append(str(parent_directory))  # 将父目录加入 Python 路径
