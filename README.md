644ff76a18bd4d0912938b721dd93dd12ebabdf8
Chunyu Li <cyli0212@gmail.com>
2023-03-04 17:15:34 +0800

    Added a combined rings test case

--- README.md
+++ README.md
@@ -28,24 +28,28 @@ pip install photfdtd
 ## Demo
 
 ```python
-from photfdtd import Sbend
-
+from photfdtd import Sbend, Grid
 
 if __name__ == "__main__":
-
     # 设置器件参数
     sbend = Sbend(
         xlength=40, ylength=60, zlength=1, x=10, y=10, z=1, direction=-1, width=10, refractive_index=1.7, name="sbend"
     )
 
     # 设置 grid 参数
-    sbend.set_grid(grid_xlength=80, grid_ylength=80, grid_zlength=1, grid_spacing=155e-9, total_time=200, pml_width=10)
+    grid = Grid(grid_xlength=80, grid_ylength=80, grid_zlength=1, grid_spacing=155e-9, total_time=200, pml_width=10)
 
     # 设置光源
-    sbend.set_source()
+    grid.set_source(
+        source_type="linesource", x=11 + sbend.xlength, xlength=0, y=sbend.y, ylength=10, period=1550e-9 / 299792458
+    )
 
-    # 保存画好的图，设置保存位置，以及从哪一个轴俯视画图
-    sbend.savefig(filepath="SbendZ.png", axis="z")
+    # 添加器件
+    grid.add_object(sbend)
+
+    # 保存图片
+    grid.savefig(filepath="SbendZ.png", axis="z")
+
 ```
 
 ### 运行结果
---
+++ tests/combined_rings_test.py
@@ -0,0 +1,48 @@
+import utils
+from photfdtd import Ring, Grid
+
+if __name__ == "__main__":
+
+    ring = Ring(
+        outer_radius=40,
+        zlength=1,
+        x=20,
+        y=20,
+        z=1,
+        width=4,
+        length=0,
+        gap=1,
+        name="ring",
+        refractive_index=3.47,
+        direction=1,
+    )
+
+    ring1 = Ring(
+        outer_radius=40,
+        zlength=1,
+        x=100,
+        y=100,
+        z=1,
+        width=4,
+        length=0,
+        gap=1,
+        name="ring1",
+        refractive_index=3.47,
+        direction=1,
+    )
+
+    grid = Grid(grid_xlength=200, grid_ylength=200, grid_zlength=1, grid_spacing=155e-9, total_time=1500, pml_width=5)
+
+    grid.set_source(
+        source_type="linesource",
+        x=16,
+        xlength=0,
+        y=ring.y - ring.width - ring.gap - 1,
+        ylength=7,
+        period=1550e-9 / 299792458,
+    )
+
+    grid.add_object(ring)
+    grid.add_object(ring1)
+
+    grid.savefig("CombinedRings.png", axis="z")
