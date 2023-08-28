5c7c9b0636069a114aa2de7591fafa58b9f4e738
TaoJia <1148777305@qq.com>
2023-08-28 11:47:14 +0800

    更新了sbend.py及其示例

--- examples/sbend_ex.py
+++ examples/sbend_ex.py
@@ -1,5 +1,4 @@
-import utils
-from photfdtd import Sbend, Grid, Solve
+from photfdtd import Sbend, Grid, Solve, constants
 
 
 if __name__ == "__main__":
@@ -8,18 +7,41 @@ if __name__ == "__main__":
 
     # 设置波导参数
     sbend = Sbend(
-        xlength=600, ylength=390, zlength=1, x=400, y=400, z=0, direction=-1, width=100, refractive_index=1.7, name="sbend",
+        xlength=200, ylength=100, zlength=20, x=100, y=75, z=13, direction=-1, width=20, refractive_index=3.47, name="sbend",
         background_index=background_index
     )
 
     # 设置 grid 参数
-    grid = Grid(grid_xlength=800, grid_ylength=800, grid_zlength=1, grid_spacing=155e-9,
-                total_time=1, foldername="test_sbend", permittivity=background_index ** 2)
+    grid = Grid(grid_xlength=200, grid_ylength=150, grid_zlength=25, grid_spacing=20e-9,
+                total_time=1200,
+                pml_width_x=20,
+                pml_width_y=10,
+                pml_width_z=1,
+                foldername="test_sbend",
+                permittivity=background_index ** 2)
 
     # 设置光源
-    # grid.set_source(
-    #     source_type="linesource", x=11 + sbend.xlength, xlength=0, y=sbend.y, ylength=10, period=1550e-9 / 299792458
-    # )
+    grid.set_source(source_type="planesource",
+                    period=1550e-9/constants.c,
+                    name="source",
+                    x=30,
+                    y=110,
+                    z=13,
+                    xlength=1,
+                    ylength=20,
+                    zlength=20
+                    )
+
+    # 设置监视器
+    grid.set_detector(detector_type="blockdetector",
+                      name="detector",
+                      x=175,
+                      y=40,
+                      z=13,
+                      xlength=1,
+                      ylength=20,
+                      zlength=20
+                      )
 
     # 添加器件
     grid.add_object(sbend)
@@ -29,10 +51,20 @@ if __name__ == "__main__":
 
     # 绘制截面
     solve._plot_(axis='z',
-                 index=-1,
+                 index=13,
                  filepath=grid.folder)
 
+    # 运行仿真
     grid.run()
 
-    # 保存图片
-    grid.save_fig(axis_number=0, axis="z")
+    # 保存仿真结果
+    grid.save_simulation()
+
+    # 绘制任意截面场图
+    grid.save_fig(axis="z",
+                  axis_number=13)
+    grid.save_fig(axis="x",
+                  axis_number=100)
+
+    # 读取仿真结果
+    data = grid.read_simulation(folder=grid.folder)
