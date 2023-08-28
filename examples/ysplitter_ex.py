03470e530514cf52cac178326ab38ac1170d4e09
TaoJia <1148777305@qq.com>
2023-08-28 15:01:34 +0800

    更新了ysplitter.py及其示例

--- examples/ysplitter_ex.py
+++ examples/ysplitter_ex.py
@@ -1,34 +1,66 @@
 import utils
-from photfdtd import Ysplitter, Grid, Solve
+from photfdtd import Ysplitter, Grid, Solve, constants
 
 if __name__ == "__main__":
 
     background_index=1.0
 
     # 设置器件参数
-    ysplitter = Ysplitter(xlength=600, ylength=600, zlength=1, x=500, y=500, z=0, direction=1, width=100, name="ysplitter",
-                          refractive_index=1.7, xlength_waveguide=200, xlength_taper=100, ylength_taper=200,
-                          width_sbend=100, background_index=background_index)
+    ysplitter = Ysplitter(xlength=200, ylength=160, zlength=20, x=100, y=100, z=13, direction=1, width=20, name="ysplitter",
+                          refractive_index=3.47, xlength_waveguide=80, xlength_taper=40, ylength_taper=40,
+                          width_sbend=20, background_index=background_index)
 
     # 设置 grid 参数
-    grid = Grid(grid_ylength=1000, grid_xlength=1000, grid_zlength=1, grid_spacing=155e-9, total_time=1,
+    grid = Grid(grid_ylength=200, grid_xlength=200, grid_zlength=25, grid_spacing=20e-9, total_time=1000,
+                pml_width_x=20,
+                pml_width_y=20,
+                pml_width_z=1,
                 foldername="test_ysplitter", permittivity=background_index ** 2)
 
     # 设置光源
-    grid.set_source(x=60, xlength=0, y=35, ylength=10, source_type="linesource", period=1550e-9 / 299792458)
+    grid.set_source(source_type="planesource",
+                    period=1550e-9 / constants.c,
+                    name="source",
+                    x=30,
+                    y=100,
+                    z=13,
+                    xlength=1,
+                    ylength=22,
+                    zlength=22
+                    )
 
+    # 设置监视器
+    grid.set_detector(detector_type="blockdetector",
+                      name="detector",
+                      x=175,
+                      y=63,
+                      z=13,
+                      xlength=1,
+                      ylength=25,
+                      zlength=22
+                      )
 
     grid.add_object(ysplitter)
 
     # 创建solve对象
     solve = Solve(grid=grid)
 
-    # 绘制x=50截面
+    # 绘制任一截面折射率分布
     solve._plot_(axis='z',
-                 index=0,
+                 index=13,
                  filepath=grid.folder)
 
+    # 运行仿真
     grid.run()
 
-    # 保存画好的图，设置保存位置，以及从哪一个轴俯视画图
-    grid.save_fig(axis="z", axis_number=0)
+    # 保存仿真结果
+    grid.save_simulation()
+
+    # 绘制任意截面场图
+    grid.save_fig(axis="z",
+                  axis_number=13)
+    grid.save_fig(axis="x",
+                  axis_number=100)
+
+    # 读取仿真结果
+    data = grid.read_simulation(folder=grid.folder)
