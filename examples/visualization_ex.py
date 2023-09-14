7608a2e9a4c2c1d6e57c0d1073e7cfe7695af235
shihao <18301172@bjtu.edu.cn>
2023-09-14 23:40:11 +0800

    <feat> Add visualize in grid.py. Add visualization_ex.py. Plot figure in Three projection plane coordinates.

--- examples/visualization_ex.py
+++ examples/visualization_ex.py
@@ -1,8 +1,9 @@
+import time
+
 from photfdtd import Waveguide, Arc, Grid, Solve, constants
 
 if __name__ == "__main__":
-
-    background_index=1.0
+    background_index = 1.0
 
     waveguide0 = Waveguide(
         xlength=50, ylength=50, zlength=50, x=50, y=50, z=40, refractive_index=3.47, name="Waveguide0",
@@ -30,7 +31,7 @@ if __name__ == "__main__":
                 pml_width_x=15,
                 pml_width_y=10,
                 pml_width_z=5,
-                permittivity=background_index ** 2,)
+                permittivity=background_index ** 2, )
 
     # 往 grid 里添加器件
     grid.add_object(waveguide0)
@@ -40,7 +41,7 @@ if __name__ == "__main__":
 
     # 设置光源
     grid.set_source(source_type="planesource",
-                    period=1550e-9/constants.c,
+                    period=1550e-9 / constants.c,
                     name="source",
                     x=20,
                     y=50,
@@ -56,11 +57,11 @@ if __name__ == "__main__":
                       x=80,
                       y=50,
                       z=40,
-                      xlength=1,
+                      xlength=0,
                       ylength=waveguide0.ylength + 8,
                       zlength=waveguide0.xlength + 8
                       )
-
+    print(grid)
     solve = Solve(grid=grid)
 
     # 绘制任一截面
@@ -78,12 +79,26 @@ if __name__ == "__main__":
     grid.save_simulation()
 
     # 绘制任意截面场图
-    grid.save_fig(axis="y",
-                  axis_number=50)
-    grid.save_fig(axis="z",
-                  axis_number=50)
-    grid.save_fig(axis="x",
-                  axis_number=50)
+    # grid.save_fig(axis="y",
+    #               axis_number=50)
+    # grid.save_fig(axis="z",
+    #               axis_number=50)
+    # grid.save_fig(axis="x",
+    #               axis_number=50)
+    stime = time.time()
+    grid.visualize(x=50, showEnergy=True, show=True, save=True)
+    sstime_1 = time.time()
+    grid.visualize(y=50, showEnergy=True, show=True, save=True)
+    sstime_2 = time.time()
+    grid.visualize(z=50, showEnergy=True, show=True, save=True)
+    sstime_3 = time.time()
+    print(sstime_1-stime)
+    print(sstime_2 - sstime_1)
+    print(sstime_3 - sstime_2)
+
+    # grid.visualize(x=50, showEnergy=False, show=True, save=True)
+    # grid.visualize(y=50, showEnergy=False, show=True, save=True)
+    # grid.visualize(z=50, showEnergy=False, show=True, save=True)
 
     # 读取仿真结果
     data = grid.read_simulation(folder=grid.folder)
