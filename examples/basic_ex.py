5e22143a78693844aba3b7c586bb553c67c4dc22
TaoJia <1148777305@qq.com>
2024-07-17 15:52:26 +0800

    Update readme

--- examples/basic_ex.py
+++ examples/basic_ex.py
@@ -9,22 +9,22 @@ if __name__ == "__main__":
     index_Re_Si, index_Im_Si = index_Si.get_refractive_index(wavelength=1.55e-6)
 
     # create the simulation region, which is a Grid object 新建一个 grid 对象
-    grid = Grid(grid_xlength=1, grid_ylength=3e-6, grid_zlength=8e-6,
+    grid = Grid(grid_xlength=3e-6, grid_ylength=1, grid_zlength=8e-6,
                 grid_spacing=20e-9,
                 permittivity=background_index ** 2,
                 foldername="basic_ex")
 
     # set waveguide 设置器件参数
     waveguide = Waveguide(
-        xlength=1, ylength=200e-9, zlength=5e-6, refractive_index=index_Re_Si, name="waveguide", grid=grid
+        xlength=200e-9, ylength=1, zlength=5e-6, refractive_index=index_Re_Si, name="waveguide", grid=grid
     )
 
     # add waveguide to grid 往 grid 里添加器件
     grid.add_object(waveguide)
 
-    # set a point source with center wl at 1550nm 设置一个点光源，波长为1550nm，波形为连续正弦
-    grid.set_source(source_type="linesource", wavelength=1550e-9, name="source", x=0, y=75, z=60,
-                    xlength=0, ylength=400e-9, zlength=0, polarization="x")
+    # set a line source with center wl at 1550nm 设置一个点光源，波长为1550nm，波形为连续正弦
+    grid.set_source(source_type="linesource", wavelength=1550e-9, name="source", x=75, y=0, z=60,
+                    xlength=400e-9, ylength=0, zlength=0, polarization="x")
 
     # # set a line detector 设置一个线监视器
     # grid.set_detector(detector_type="linedetector",
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     Grid.dB_map(grid=grid, field="E", field_axis="x")
 
     # # 绘制仿真结束时刻空间场分布
-    Grid.plot_field(grid=grid, field="E", field_axis="x", axis="x", axis_index=0)
+    Grid.plot_field(grid=grid, field="E", field_axis="x", axis="y", axis_index=0, vmin=-1, vmax=1)
 
     # # 如果添加了监视器，还可以绘制某一点时域场变化曲线，这里选择index=30即监视器中心
     # Grid.plot_fieldtime(folder=grid.folder, data=data, field_axis="z", index=30, name_det="detector")
